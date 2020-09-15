f8c7eeff0d58d4aa40f4fdeb3ea32455fadfa4ad
Jason Tian <tianjiasen@outlook.com>
2020-09-15 23:01:00 +0800

    Create Dyna+GCNv2_run.sh

---
+++ Dyna+GCNv2_run.sh
@@ -0,0 +1,26 @@
+python associate.py rgb.txt depth.txt > associations.txt
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_halfsphere/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_halfsphere/result/output
+
+USE_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_halfsphere/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_halfsphere/result/output
+
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere/result/output_GCN_1
+
+USE_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_halfsphere/result/output_ORB
+
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz/result/output_GCN
+
+
+USE_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_xyz/result/output_ORB
+
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy/result/output_GCN
+
+USE_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_walking_rpy/result/output_ORB
+
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/result/output_GCN
+
+USE_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/RGB-D/gcn2_640x480.pt ./Examples/RGB-D/rgbd_tum Vocabulary/GCNvoc.bin Examples/RGB-D/TUM3.yaml /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/associations.txt /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/result/mask /home/jiasen/Evaluation/dynamic/rgbd_dataset_freiburg3_sitting_xyz/result/output_ORB
