564d6c822f98e34b5a95d3ec7fa2e7555b58b739
Jason Tian <tianjiasen@outlook.com>
2020-09-15 23:01:47 +0800

    Add files via upload

---
+++ Fisheye+GCNv2_run.sh
@@ -0,0 +1,20 @@
+FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/GCNvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room1_512_16/mav0/cam1
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/GCNvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room2_512_16/mav0/cam1
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/GCNvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room3_512_16/mav0/cam1
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/GCNvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room4_512_16/mav0/cam1
+
+FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/GCNvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-magistrale5_512_16/mav0/cam1
+
+USB_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/ORBvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room1_512_16/mav0/cam1
+
+USB_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/ORBvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room2_512_16/mav0/cam1
+
+USB_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/ORBvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room3_512_16/mav0/cam1
+
+USB_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/ORBvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-room4_512_16/mav0/cam1
+
+USB_ORB=1 FULL_RESOLUTION=1 GCN_PATH=Examples/Monocular/gcn2_640x480.pt ./Examples/Monocular/fisheye_GCNv2_mono_tum Vocabulary/ORBvoc.bin Examples/Monocular/fisheye_eucm_cam1.yaml /home/jiasen/Evaluation/fisheye/dataset-magistrale5_512_16/mav0/cam1
+
